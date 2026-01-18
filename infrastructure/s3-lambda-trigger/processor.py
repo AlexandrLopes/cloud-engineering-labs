79f2fb025b5f028e0a8c16333c062a7f61abd5ed
Alexandre Lopes <alexandre.w.d.lopes@gmail.com>
2026-01-18 14:59:41 -0300

    Feat: Add SecOps inout validation (size and extension check)

--- infrastructure/s3-lambda-trigger/processor.py
+++ infrastructure/s3-lambda-trigger/processor.py
@@ -1,42 +1,65 @@
 import json
 import urllib.parse
 import boto3
-import time
+import os
+
+# Security configuration
+ALLOWED_EXTENSIONS = {'.pdf', '.txt', '.csv', '.json', '.png', '.jpg'}
+MAX_SIZE_BYTES = 5 * 1024 * 1024 # 5 MB
 
 dynamodb = boto3.resource('dynamodb')
 table_name = "S3_File_Audit_Log"
 table = dynamodb.Table(table_name)
 
+def is_safe_file(filename, size):
+    # 1. Extension Validation (File Type)
+    ext = os.path.splitext(filename)[1].lower()
+    if ext not in ALLOWED_EXTENSIONS:
+        return False, f"Extension {ext} not allowed"
+    
+    # 2. Size Validation (DoS Protection)
+    if size > MAX_SIZE_BYTES:
+        return False, f"File too large ({size} bytes)"
+        
+    return True, "Safe"
+
 def handler(event, context):
-    print(" Event received from S3!")
+    print(" SecOps Lambda starting...")
     
     for record in event['Records']:
-        # Extract event data
         bucket_name = record['s3']['bucket']['name']
         file_name = urllib.parse.unquote_plus(record['s3']['object']['key'])
         size = record['s3']['object']['size']
         event_time = record['eventTime']
         
-        print(f"Processing: {file_name} ({size} bytes)")
+        print(f" Inspecting: {file_name} ({size} bytes)")
 
-        #  DynamoDB
+        is_safe, reason = is_safe_file(file_name, size)
+        
+        status = 'PROCESSED' if is_safe else 'BLOCKED'
+        
         try:
-            response = table.put_item(
-                Item={
-                    'file_name': file_name,     # Primary Key 
-                    'bucket': bucket_name,
-                    'size_bytes': size,
-                    'upload_time': event_time,
-                    'status': 'PROCESSED'
-                }
-            )
-            print(f" Saved to DynamoDB: {file_name}")
+            item = {
+                'file_name': file_name,
+                'bucket': bucket_name,
+                'size_bytes': size,
+                'upload_time': event_time,
+                'status': status
+            }
+            
+            if not is_safe:
+                item['block_reason'] = reason
+                print(f"🚫 BLOCKED: {file_name} - {reason}")
+            else:
+                print(f" APPROVED: {file_name}")
+
+            table.put_item(Item=item)
             
         except Exception as e:
-            print(f" Error writing to DynamoDB: {str(e)}")
-            raise e # Force Lambda to fail so CloudWatch notify you
+            print(f"❌ Error writing to DB: {str(e)}")
+            raise e
         
     return {
         'statusCode': 200,
-        'body': json.dumps('File metadata indexed successfully')
+        'body': json.dumps('Security scan complete')
     }
