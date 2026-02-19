4a585dd7f280191133a788179fdc2c3d012468c8
Alexandre Lopes <alexandre.w.d.lopes@gmail.com>
2026-02-19 18:04:42 -0300

    fix: apply security hardening to remote backend to satisfy Trivy

--- infrastructure/remote-backend/main.tf
+++ infrastructure/remote-backend/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. Bucket S3 para guardar o arquivo de estado
+# 1. Bucket S3
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "alexandre-labs-terraform-state-2026" # Lembre-se: Nomes de bucket S3 precisam ser únicos no mundo! Se der erro, coloque uns números aleatórios no final.
+  bucket        = "alexandre-labs-terraform-state-2026"
   force_destroy = true 
 }
 
@@ -15,7 +15,26 @@ resource "aws_s3_bucket_versioning" "state_versioning" {
   }
 }
 
-# 2. Tabela DynamoDB para o State Lock
+# 🛡️ SEGURANÇA S3 (FIX TRIVY): Criptografia
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# 🛡️ SEGURANÇA S3 (FIX TRIVY): Bloqueio Público
+resource "aws_s3_bucket_public_access_block" "state_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# 2. Tabela DynamoDB
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-locks"
   billing_mode = "PAY_PER_REQUEST"
@@ -24,5 +43,13 @@ resource "aws_dynamodb_table" "terraform_locks" {
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+  
+  point_in_time_recovery {
+    enabled = true
   }
 }
